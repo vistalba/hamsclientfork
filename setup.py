95ad9e85586966fed5428d9cf5b18b04e9de204e
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2025-01-10 12:38:38 +0000

    Tag 0.2.19.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="hamsclientfork",
-    version="0.2.18",
+    version="0.2.19",
     url="https://github.com/Rudd-O/hamsclientfork",
     license="MIT",
     author="websylv",
